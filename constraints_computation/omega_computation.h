45e0d85fc69651d97cdbe42fafe092900ab1c067
Daniele Morra <morradaniele@ymail.com>
2022-01-28 13:58:33 +0100

    added omega computation for the constraints of MPC

---
+++ constraints_computation/omega_computation.h
@@ -0,0 +1,25 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// omega_computation.h
+//
+// Code generation for function 'omega_computation'
+//
+
+#ifndef OMEGA_COMPUTATION_H
+#define OMEGA_COMPUTATION_H
+#define NO_FLOATS
+
+// Include files
+#include <cstddef>
+#include <cstdlib>
+#include<limits>
+
+// Function Declarations
+extern void omegaComputation(double* in1, double* omega, void* user_data);
+void Test_omegaComputation(double* in1, double* omega);
+
+#endif
+// End of code generation (omega_computation.h)
